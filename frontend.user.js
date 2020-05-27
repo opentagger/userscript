66b58fdaa284d819e90ae282868bba0fc89f8d01
noirscape <neko@catgirlsin.space>
2020-05-27 14:37:26 +0200

    Frontend: no more dupe arguments

--- frontend.user.js
+++ frontend.user.js
@@ -12,44 +12,47 @@ let requestURL = '__BASEURL__/bulk_users';
 let re = "https?:\/\/(?:.*\.)?reddit.com\/u(?:ser)?\/(.*)"
 
 function massRequestUsername(usernames, callback) {
-  let url = new URL(requestURL);
-  
-  usernames.forEach(function(username) {
-    url.searchParams.append('usernames', username)
-  });
+    let url = new URL(requestURL);
 
-  GM.xmlHttpRequest({
-    method: "GET",
-    url: url,
-    onload: callback
-  });
+    usernames.forEach(function(username) {
+        url.searchParams.append('usernames', username)
+    });
+
+    GM.xmlHttpRequest({
+        method: "GET",
+        url: url,
+        onload: callback
+    });
 }
 
 function getAllAuthorsFromPage() {
 	let usernames = []
-  document.querySelectorAll(".author").forEach(function(node) {
-    let username = node.href.match(re)
-    usernames.push(username[1]);
+    document.querySelectorAll(".author").forEach(function(node) {
+        let username = node.href.match(re)
+
+        if(usernames.indexOf(username) === -1) {
+            usernames.push(username[1]);
+        }
 	});
-  return usernames
+    return usernames
 }
 
 function injectAfterAllAuthorsFromPage(responseJSON) {
-  document.querySelectorAll(".author").forEach(function(node) {
-    let username = node.href.match(re);
-    
-    var el = document.createElement("span");    
-  	el.innerHTML = responseJSON[username[1]];
-    console.log(el);
-    node.parentNode.insertBefore(el, node.nextSibling);
+    document.querySelectorAll(".author").forEach(function(node) {
+        let username = node.href.match(re);
+
+        var el = document.createElement("span");    
+        el.innerHTML = responseJSON[username[1]];
+        console.log(el);
+        node.parentNode.insertBefore(el, node.nextSibling);
   });  
 }
 
 function main() {
-  let usernames = getAllAuthorsFromPage()
-  massRequestUsername(usernames, function(response) {
-  	injectAfterAllAuthorsFromPage(JSON.parse(response.responseText).results);
-  });
+    let usernames = getAllAuthorsFromPage()
+    massRequestUsername(usernames, function(response) {
+        injectAfterAllAuthorsFromPage(JSON.parse(response.responseText).results);
+    });
 }
 
 main();
