9acdc5f6a51041f1ce9bd053b1474d961dc2018c
noirscape <neko@catgirlsin.space>
2020-05-27 14:13:12 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+## usertagger-frontend
+
+This is the frontend for usertagger.
+
+### Deployment
+
+Normally this path is set by the server application (and a functional version of this userscript is bundled with the server application). If you're somehow deploying this separately, change `__BASEURL__` to that of the location of the server you are using.
---
+++ frontend.user.js
@@ -0,0 +1,55 @@
+// ==UserScript==
+// @name     Usertagger
+// @version  1
+// @include  http://*.reddit.com/*
+// @include  http://reddit.com/*
+// @include  https://*.reddit.com/*
+// @include  https://reddit.com/*
+// @grant    GM.xmlHttpRequest
+// ==/UserScript==
+
+let requestURL = '__BASEURL__/bulk_users';
+let re = "https?:\/\/(?:.*\.)?reddit.com\/u(?:ser)?\/(.*)"
+
+function massRequestUsername(usernames, callback) {
+  let url = new URL(requestURL);
+  
+  usernames.forEach(function(username) {
+    url.searchParams.append('usernames', username)
+  });
+
+  GM.xmlHttpRequest({
+    method: "GET",
+    url: url,
+    onload: callback
+  });
+}
+
+function getAllAuthorsFromPage() {
+	let usernames = []
+  document.querySelectorAll(".author").forEach(function(node) {
+    let username = node.href.match(re)
+    usernames.push(username[1]);
+	});
+  return usernames
+}
+
+function injectAfterAllAuthorsFromPage(responseJSON) {
+  document.querySelectorAll(".author").forEach(function(node) {
+    let username = node.href.match(re);
+    
+    var el = document.createElement("span");    
+  	el.innerHTML = responseJSON[username[1]];
+    console.log(el);
+    node.parentNode.insertBefore(el, node.nextSibling);
+  });  
+}
+
+function main() {
+  let usernames = getAllAuthorsFromPage()
+  massRequestUsername(usernames, function(response) {
+  	injectAfterAllAuthorsFromPage(JSON.parse(response.responseText).results);
+  });
+}
+
+main();
